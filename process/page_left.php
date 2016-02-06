ab053632cd753b7887394c4c24752efa26fb84e7
Poom Tammawutikul <poomlak1994@hotmail.com>
2016-02-07 02:13:42 +0700

    format facebook like page style

--- process/page_left.php
+++ process/page_left.php
@@ -8,7 +8,19 @@ function facebook_block() {
 		<div class="panel panel-default">
 			<div class="panel-heading">Facebook</div>
 			<div class="panel-body" style="padding:0">
-				<div class="fb-page" data-href="https://www.facebook.com/kmitlhorpak" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/kmitlhorpak"><a href="https://www.facebook.com/kmitlhorpak">Kmitlhorpak</a></blockquote></div></div>
+				<div class="fb-page" 
+					data-href="https://www.facebook.com/kmitlhorpak" 
+					data-tabs="timeline" 
+					data-small-header="false" 
+					data-adapt-container-width="true" 
+					data-hide-cover="false" 
+					data-show-facepile="true">
+					<div class="fb-xfbml-parse-ignore">
+						<blockquote cite="https://www.facebook.com/kmitlhorpak">
+							<a href="https://www.facebook.com/kmitlhorpak">Kmitlhorpak</a>
+						</blockquote>
+					</div>
+				</div>
 			</div>
 		</div>
 <?php
