76c99eac9a6cb702c03f122d2138faf877092a39
Poom Tammawutikul <poomlak1994@hotmail.com>
2016-02-06 15:00:18 +0700

    Add Facebook like box

--- process/page_left.php
+++ process/page_left.php
@@ -2,25 +2,26 @@
 facebook_block();
 ad_block();	
 
+function facebook_block() {
+
+?>
+		<div class="panel panel-default">
+			<div class="panel-heading">Facebook</div>
+			<div class="panel-body" style="padding:0">
+				<div class="fb-page" data-href="https://www.facebook.com/kmitlhorpak" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/kmitlhorpak"><a href="https://www.facebook.com/kmitlhorpak">Kmitlhorpak</a></blockquote></div></div>
+			</div>
+		</div>
+<?php
+}
+
 function ad_block() {
-	echo '
+?>
 		<div class="panel panel-default">
 			<div class="panel-heading">Advertisment</div>
 			<div class="panel-body">
 				<br><br><br><br><br><br><br><br>
 			</div>
 		</div>
-	';
-}
-
-function facebook_block() {
-	echo '
-		<div class="panel panel-default">
-			<div class="panel-heading">Facebook</div>
-			<div class="panel-body">
-				<br><br><br><br><br><br><br><br>
-			</div>
-		</div>
-	';
+<?php
 }
 ?>
